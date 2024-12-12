b2d2cb65c855de96dedfdef1cce24d49660ab4b2
naveengavvala28 <145005663+naveengavvala28@users.noreply.github.com>
2024-12-12 17:53:38 +0530

    Add files via upload

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import {config} from "dotenv"
+config({path:"./.env.local"});
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./utils/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
+  },
+};
